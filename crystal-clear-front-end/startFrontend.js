e53e3d773725425293fb61d3a6e19a09f8dc3921
Jon Mendizabal <jonmendi@gmail.com>
2024-02-19 11:16:14 +0100

    add detect-port to dependencies

--- crystal-clear-front-end/startFrontend.js
+++ crystal-clear-front-end/startFrontend.js
@@ -1,18 +1,14 @@
-// startFrontend.js
 const detectPort = require("detect-port");
 const { exec } = require("child_process");
 
-// Check if the default React port 3000 is available
 detectPort(3000, (err, availablePort) => {
   if (err) {
     console.error(err);
     process.exit(1);
   }
 
-  // Use the available port found by detect-port, or the default port if it's free
   const port = availablePort;
 
-  // Start the React app on the available port using cross-env
   exec(`cross-env PORT=${port} npm start`, (err, stdout, stderr) => {
     if (err) {
       console.error(`exec error: ${err}`);
