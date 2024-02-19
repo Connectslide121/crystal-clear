56781d674234ce7fc2f58b6b5ff00fdb768e1287
Jon Mendizabal <jonmendi@gmail.com>
2024-02-19 10:44:18 +0100

    Add Unit tests

---
+++ crystal-clear-back-end/Tests/CitiesControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Services.DTOs;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class CitiesControllerTests
+    {
+        [Fact]
+        public void GetCities_ReturnsOkResult_WithListOfCities()
+        {
+            var mockService = new Mock<ICitiesService>();
+            mockService.Setup(service => service.GetCities())
+                       .Returns(new List<CityDTO> { new CityDTO(), new CityDTO() }); 
+
+            var controller = new CitiesController(mockService.Object);
+
+            var result = controller.GetCities();
+
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var returnValue = Assert.IsType<List<CityDTO>>(okResult.Value);
+            Assert.Equal(2, returnValue.Count); 
+        }
+
+        [Fact]
+        public void GetCities_ReturnsNotFound_WhenNoCitiesExist()
+        {
+            var mockService = new Mock<ICitiesService>();
+            mockService.Setup(service => service.GetCities())
+                       .Returns((List<CityDTO>)null);
+
+            var controller = new CitiesController(mockService.Object);
+
+            var result = controller.GetCities();
+
+            Assert.IsType<NotFoundResult>(result);
+        }
+    }
+}
---
+++ crystal-clear-back-end/Tests/QuotesControllerTests.cs
@@ -0,0 +1,86 @@
+﻿using Core;
+using DatabaseConnection;
+using Microsoft.EntityFrameworkCore;
+using Services.DTOs;
+using Services.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class QuotesControllerTests
+    {
+        private readonly QuotesService _quotesService;
+        private readonly AppDbContext _dbContext;
+
+        public QuotesControllerTests()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "CrystalClear")
+                .Options;
+
+            _dbContext = new AppDbContext(options);
+
+            _quotesService = new QuotesService(_dbContext);
+        }
+
+        private void SeedDatabase()
+        {
+            _dbContext.Cities.AddRange(new[]
+            {
+            new City { Id = 1, PricePerSquareMeter = 65, Name = "ExampleCity" }
+        });
+            _dbContext.Options.AddRange(new[]
+            {
+            new Option { Id = 1, Price = 100, Name = "ExampleOption1" },
+            new Option { Id = 2, Price = 600, Name = "ExampleOption2" }
+        });
+
+            _dbContext.SaveChanges();
+        }
+
+        [Fact]
+        public async Task CreateQuote_WithValidParameters_ReturnsQuoteDTO()
+        {
+            SeedDatabase();
+
+            var city = new CityDTO { PricePerSquareMeter = 10, Id = 1 };
+            var options = new List<OptionDTO>
+        {
+            new OptionDTO { Price = 5, Id = 1 },
+            new OptionDTO { Price = 15, Id = 2 }
+        };
+            int squareMeters = 50;
+            int expectedTotalPrice = city.PricePerSquareMeter * squareMeters + options[0].Price + options[1].Price;
+
+            var result = await _quotesService.CreateQuote(city, options, squareMeters);
+
+            Assert.NotNull(result);
+            Assert.Equal(expectedTotalPrice, result.TotalPrice);
+        }
+
+        [Fact]
+        public async Task CreateQuote_WithNoOptions_ReturnsQuoteDTOWithBasePriceOnly()
+        {
+
+            var city = new CityDTO { PricePerSquareMeter = 10, Id = 1 };
+            var options = new List<OptionDTO>();
+            int squareMeters = 50;
+            int expectedTotalPrice = city.PricePerSquareMeter * squareMeters;
+
+            var result = await _quotesService.CreateQuote(city, options, squareMeters);
+
+            Assert.NotNull(result);
+            Assert.Equal(expectedTotalPrice, result.TotalPrice);
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Database.EnsureDeleted();
+            _dbContext.Dispose();
+        }
+    }
+}
