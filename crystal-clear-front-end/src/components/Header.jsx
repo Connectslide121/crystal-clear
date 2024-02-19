0206ded5bf37ec9a73fb71041a5f654600bd59c1
Jon Mendizabal <jonmendi@gmail.com>
2024-02-19 12:59:25 +0100

    delete context

--- crystal-clear-front-end/src/components/Header.jsx
+++ crystal-clear-front-end/src/components/Header.jsx
@@ -1,20 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "../images/logo.webp";
 import { Outlet } from "react-router-dom";
-import { QuoteContext } from "../context";
 
 export default function Header() {
-  const [quote, setQuote] = useState({});
-
   return (
     <div id="app">
       <section id="hero">
         <img src={logo} alt="logo" className="logo" />
       </section>
       <main>
-        <QuoteContext.Provider value={{ quote, setQuote }}>
-          <Outlet />
-        </QuoteContext.Provider>
+        <Outlet />
       </main>
     </div>
   );
--- crystal-clear-front-end/src/context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-export const QuoteContext = createContext({
-  quote: "",
-  setQuote: () => {}
-});
